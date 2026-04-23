62df8aebdb6a4d09ef135ff7dc957c42e2b762db
Fotsopliya <dfotso2015@gmail.com>
2026-04-23 16:06:48 +0100

    refactor: simplify DashboardLayout component structure and remove unused comments

--- app/[locale]/(dashboard)/layout.tsx
+++ app/[locale]/(dashboard)/layout.tsx
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
-import { Header } from '@/components/layout/header'
-import { Sidebar } from '@/components/layout/sidebar'
-import React from 'react'
+import { Header } from "@/components/layout/header";
+import { Sidebar } from "@/components/layout/sidebar";
 
-function DashboardLayout({ children }: {
-    children: React.ReactNode
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode;
 }) {
-
     return (
         <div className="flex w-full h-full overflow-hidden">
-            {/* Sidebar - Fixed Width */}
             <Sidebar />
-
-            {/* Main Content Area */}
             <div className="flex-1 flex flex-col min-w-0 bg-slate-100 relative">
                 <Header />
                 <main className="flex-1 p-4 overflow-auto min-h-0">
@@ -20,32 +16,5 @@ function DashboardLayout({ children }: {
                 </main>
             </div>
         </div>
-    )
+    );
 }
-
-export default DashboardLayout
-=======
-import { Header } from "@/components/layout/header";
-import { Sidebar } from "@/components/layout/sidebar";
-
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="flex w-full h-full overflow-hidden">
-      {/* Sidebar - Fixed Width */}
-      <Sidebar />
-
-      {/* Main Content Area */}
-      <div className="flex-1 flex flex-col min-w-0 bg-slate-100 relative">
-        <Header />
-        <main className="flex-1 p-4 overflow-auto min-h-0">
-          {children}
-        </main>
-      </div>
-    </div>
-  );
-}
->>>>>>> main
